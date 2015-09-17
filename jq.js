704e68096304963efdf0a6cca06b89ed143b6ddf
Steven Etter <tadjo20@yahoo.com>
2015-09-16 22:53:34 -0600

    stage 1, playground, player info and playscreen set up

--- jq.js
+++ jq.js
@@ -2,15 +2,27 @@ $(document).ready(function () {
     console.log("ready");
 
     $("#playground").click(function (e) {
-        console.log("click " + e.pageX + "  " + e.pageY);
+        //console.log("click " + e.pageX + "  " + e.pageY);
         //var offset = $(this).offset();
         //alert(e.pageX - offset.left);
         //alert(e.pageY - offset.top);
-        var item = [   X  = e.pageX,
-         Y = e.pageY];
 
 
-        console.log(item[0]);
+
+
+        var location = {xAxis: e.clientX, yAxis: e.clientY};
+
+
+        $("#circle").css({
+            "visibility": "visible",
+            //"background":"",
+            "position":"absolute",
+            "left":location.xAxis,
+            "top":location.yAxis
+        });
+
+        console.log("click " );
+
     })
 
 
