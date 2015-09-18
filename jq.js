38e2519590e5caf3f2c67843edfdc52415d3f197
Steven Etter <tadjo20@yahoo.com>
2015-09-17 21:28:18 -0600

    work beat

--- jq.js
+++ jq.js
@@ -6,22 +6,27 @@ $(document).ready(function () {
         //var offset = $(this).offset();
         //alert(e.pageX - offset.left);
         //alert(e.pageY - offset.top);
+        var playernumber = $("#playerNumber").text();
+        var playermoves = $("#playerMoves").text();
+
+        while (playermoves != "0") {
+            var location = { xAxis: e.clientX, yAxis: e.clientY };
+
+
+            $("#circle").css({
+                "visibility": "visible",
+                //"background":"",
+                "position": "absolute",
+                "left": location.xAxis - 14,
+                "top": location.yAxis - 14
+            });
+            //playermoves.setText($("playerMoves").text((parseInt(playermoves) - 1).toString()));
+            //$("playerMoves").text((parseInt(playermoves) - 1).toString());
+        }
 
 
 
-
-        var location = {xAxis: e.clientX, yAxis: e.clientY};
-
-
-        $("#circle").css({
-            "visibility": "visible",
-            //"background":"",
-            "position":"absolute",
-            "left":location.xAxis,
-            "top":location.yAxis
-        });
-
-        console.log("click " );
+        console.log("click " + playernumber + playermoves);
 
     })
 
