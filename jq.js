84c0e96ca33520eddbfb2096d88cf220a3afdd87
Steven Etter <tadjo20@yahoo.com>
2015-09-19 11:07:15 -0600

    fixed if loop, moves decrementing

--- jq.js
+++ jq.js
@@ -9,10 +9,10 @@ $(document).ready(function () {
         var playernumber = $("#playerNumber").text();
         var playermoves = $("#playerMoves").text();
 
-        while (playermoves != "0") {
+        if (playermoves != "0") {
             var location = { xAxis: e.clientX, yAxis: e.clientY };
 
-
+            console.log("click if " + playernumber + "  " + playermoves);
             $("#circle").css({
                 "visibility": "visible",
                 //"background":"",
@@ -20,8 +20,11 @@ $(document).ready(function () {
                 "left": location.xAxis - 14,
                 "top": location.yAxis - 14
             });
-            //playermoves.setText($("playerMoves").text((parseInt(playermoves) - 1).toString()));
+            //playermoves.text($("playerMoves").text((parseInt(playermoves) - 1).toString()));
             //$("playerMoves").text((parseInt(playermoves) - 1).toString());
+            //$("playerMoves").text().toString();
+            $("#playerMoves").text( (parseInt($("#playerMoves").text()) -1  ));
+            console.log( (parseInt($("#playerMoves").text()) -1  ));
         }
 
 
